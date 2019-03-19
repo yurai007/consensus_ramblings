3df7c94032107c12a6af5d6637cff5ec87d1b4d7
yurai007 <dawid_jurek@vp.pl>
2019-03-19 15:07:19 +0100

    add 2PC sketch

---
+++ 2pc.kt
@@ -0,0 +1,108 @@
+﻿
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import kotlin.system.measureTimeMillis
+
+/* Two Phase Commit
+   ref: https://www.the-paper-trail.org/post/2008-11-27-consensus-protocols-two-phase-commit/
+   TODO1: why lack of launches arround run-s cause very strange behaviour (10s timeout without any print :) ?
+   TODO2: kotlinc -Xuse-experimental=kotlin.Experimental 2pc.kt -d 2pc.jar
+   error: unresolved reference: kotlinx
+*/
+
+enum class State {
+    INITIAL, PROPOSE, VOTE, COMMIT_OR_ABORT
+}
+
+abstract class Node {
+    abstract suspend fun run()
+    var register: Int = 0
+    var state: State = State.INITIAL
+}
+
+class Replica(shouldCommit : Boolean) : Node() {
+    override suspend fun run() {
+        assert(state == State.INITIAL)
+        val proposedValue = proposeFromLeader()
+        state = State.PROPOSE
+        vote(shouldCommit)
+        state = State.VOTE
+        val commitReply = commitOrAbortFromLeader()
+        state = State.COMMIT_OR_ABORT
+        if (commitReply) {
+            register = proposedValue
+            println("Replica: Value := $register")
+        } else {
+            println("No consensus")
+        }
+    }
+
+    fun addLeader(leader_ : Leader) = {
+        leader = leader_
+    }
+
+    suspend fun commitOrAbortFromLeader() = channelToLeader.receive().toBoolean()
+
+    suspend fun commitOrAbort(value : Boolean) = channelToLeader.send(value.toInt())
+
+    suspend fun voteFromReplica() : Boolean = channelToLeader.receive().toBoolean()
+
+    suspend fun vote(value : Boolean) = channelToLeader.send(value.toInt())
+
+    suspend fun proposeFromLeader() : Int = channelToLeader.receive()
+
+    suspend fun propose(value : Int) = channelToLeader.send(value)
+
+    fun Boolean.toInt() = if (this) 1 else 0
+    fun Int.toBoolean() = if (this == 1) true else false
+
+    var leader : Leader? = null
+    val shouldCommit = shouldCommit
+    val channelToLeader = Channel<Int>()
+}
+
+class Leader(replicas : List<Replica>, proposed : Int) : Node() {
+    override suspend fun run() {
+        assert(state == State.INITIAL)
+        state = State.PROPOSE
+        replicas.forEach {it.propose(proposedValue)}
+        state = State.VOTE
+        val votes = mutableListOf<Boolean>()
+        replicas.forEach {votes.add(it.voteFromReplica())}
+        state = State.COMMIT_OR_ABORT
+        val maybeCommit = !votes.contains(false)
+        replicas.forEach {it.commitOrAbort(maybeCommit)}
+        if (maybeCommit) {
+            register = proposedValue
+            println("Leader: Value := $register")
+        } else {
+            println("No consensus")
+        }
+        state = State.INITIAL
+    }
+
+    val replicas = replicas
+    val proposedValue = proposed
+}
+
+fun oneLeaderOneReplicaScenarioWithConsensus() = runBlocking<Unit> {
+    val replica = Replica(true)
+        val leader = Leader(listOf(replica), 123)
+    replica.addLeader(leader)
+    launch { replica.run() }
+    launch { leader.run() }
+}
+
+fun oneLeaderOneReplicaScenarioNoConsensus() = runBlocking<Unit> {
+    val replica = Replica(false)
+        val leader = Leader(listOf(replica), 123)
+    replica.addLeader(leader)
+    launch { replica.run() }
+    launch { leader.run() }
+}
+
+fun main(args: Array<String>) {
+    oneLeaderOneReplicaScenarioWithConsensus()
+    oneLeaderOneReplicaScenarioNoConsensus()
+}
