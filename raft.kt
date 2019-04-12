e1b513e7d62ca9d77c8708b2b32638553402ffef
yurai007 <dawid_jurek@vp.pl>
2019-04-12 14:21:41 +0200

    even more Raft

--- raft.kt
+++ raft.kt
@@ -10,7 +10,7 @@ enum class State {
 
 interface Message
 data class HeartBeat(val dummy : Boolean = true) : Message
-data class AppendEntriesReq(val entry : Pair<Char, Int>, val term : Int, val prevIndex : Int, val prevTerm : Int) : Message
+data class AppendEntriesReq(val entry : Pair<Char, Int>, val term : Int, val prevIndex : Int, val prevTerm : Int, val leaderCommit : Int) : Message
 data class AppendEntriesResp(val term : Int, val success : Boolean) : Message
 data class InternalLogEntry(val entry : Pair<Char, Int>, val term : Int)
 
@@ -20,19 +20,38 @@ abstract class Node {
     val log = mutableListOf<InternalLogEntry>()
     var state: State = State.INITIAL
     var currentTerm = 0
+    var commitIndex = 0
+    var lastApplied = 0
 }
 
 class Follower() : Node() {
 
     override suspend fun run() {
         assert(state == State.INITIAL)
+        currentTerm++
         val heartbeat = receiveHeartbeat()
         val appendEntries = receiveAppendEntriesReq()
+        // [1]
         if (appendEntries.term < currentTerm) {
             sendAppendEntriesResp(AppendEntriesResp(currentTerm, false))
         }
+        val index = appendEntries.prevIndex
+        if (index < log.size) {
+            // [2]
+            if (log.get(index).term != appendEntries.prevTerm) {
+                sendAppendEntriesResp(AppendEntriesResp(currentTerm, false))
+            }
+        }
+        if (index + 1 < log.size) {
+            // [3]
+            if (log.get(index + 1).term != appendEntries.term) {
+                // TODO: delete the existing entry and all that follow it
+            }
+        }
         val (id, value) = appendEntries.entry
         if (false) {
+            commitIndex++
+            logState.set(id, value)
             println("Follower: $id := $value")
         } else {
             println("No consensus for $id")
@@ -53,16 +72,24 @@ class Follower() : Node() {
 class Leader(followers : List<Follower>, entriesToReplicate : HashMap<Char, Int>) : Node() {
     override suspend fun run() {
         assert(state == State.INITIAL)
+        currentTerm++
         followers.forEach {it.sendHeartbeat()}
         entriesToReplicate.forEach { (id, value) ->
-                currentTerm++
-                logState.set(id, value)
                 val entry = Pair<Char, Int>(id, value)
+                var prevIndex = 0
+                var prevTerm = 0
+                if (!log.isEmpty()) {
+                    prevIndex = log.size - 1
+                    val lastEntry = log.get(prevIndex)
+                    prevTerm = lastEntry.term
+                }
                 log.add(InternalLogEntry(entry, currentTerm))
-                followers.forEach {it.sendAppendEntriesReq(AppendEntriesReq(entry, currentTerm, 0, 0))}
+                followers.forEach {it.sendAppendEntriesReq(AppendEntriesReq(entry, currentTerm, prevIndex, prevTerm, commitIndex))}
                 val responses = mutableListOf<AppendEntriesResp>()
                 followers.forEach {responses.add(it.receiveAppendEntriesResp())}
                 if (false) {
+                    commitIndex++
+                    logState.set(id, value)
                     println("Leader: $id := $value")
                 } else {
                     println("No consensus for $id")
@@ -75,10 +102,25 @@ class Leader(followers : List<Follower>, entriesToReplicate : HashMap<Char, Int>
     val nextIndex = HashMap<Follower, Int>()
     val matchIndex = HashMap<Follower, Int>()
     val entriesToReplicate = entriesToReplicate
-    var commitIndex = 0
 }
 
 fun oneLeaderOneFollowerScenarioWithConsensus() = runBlocking<Unit> {
+    val followers = listOf(Follower())
+    val entriesToReplicate = hashMapOf('x' to 1, 'y' to 2)
+    val leader = Leader(followers, entriesToReplicate)
+    launch { leader.run() }
+    followers.forEach { launch { it.run() } }
+}
+
+fun oneLeaderOneFollowerMoreEntriesScenarioWithConsensus() = runBlocking<Unit> {
+    val followers = listOf(Follower())
+    val entriesToReplicate = hashMapOf('x' to 1, 'y' to 2, 'x' to 3, 'z' to 2, 'y' to 1, 'y' to 3)
+    val leader = Leader(followers, entriesToReplicate)
+    launch { leader.run() }
+    followers.forEach { launch { it.run() } }
+}
+
+fun oneLeaderManyFollowersScenarioWithConsensus() = runBlocking<Unit> {
     val followers = listOf(Follower(), Follower(), Follower(), Follower(), Follower(), Follower(), Follower(),
                  Follower(), Follower(), Follower(), Follower(), Follower(), Follower())
     val entriesToReplicate = hashMapOf('x' to 1, 'y' to 2)
@@ -89,4 +131,6 @@ fun oneLeaderOneFollowerScenarioWithConsensus() = runBlocking<Unit> {
 
 fun main(args: Array<String>) {
     oneLeaderOneFollowerScenarioWithConsensus()
+    oneLeaderOneFollowerMoreEntriesScenarioWithConsensus()
+    oneLeaderManyFollowersScenarioWithConsensus()
 }
